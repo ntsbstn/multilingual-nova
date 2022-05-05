c675a35632bb2b12289345e23293482d7ac58a46
Binh Nguyen <nguyen.binh@ebuynow.com>
2022-05-05 13:43:31 +0700

    null fix

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -28,9 +28,16 @@ class FieldServiceProvider extends PackageServiceProvider
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'nova-language-tool');
 
         $lang = request('lang', request()->header('lang'));
-        
+        $requestUrl = parse_url(request()->header('referer'));
+        if(isset($requestUrl['query'])){
+            parse_str($requestUrl['query'], $params);
+            $lang = $params['lang'] ?? request()->language;
+        }
         if ($lang) {
-            app()->setLocale($lang);
+            App::setLocale($lang);
+        }
+        if($lang = request('lang', request()->header('lang'))){
+            App::setLocale($lang);
         }
 
         Nova::serving(function () {
